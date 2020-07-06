c8a3367341a88bf514eecc60c0aee6dc0bdbdeb4
weakfantasy <51085683+weakfantasy@users.noreply.github.com>
2020-07-06 22:23:56 +0200

    MinMaxScale, komentari

--- project2/credit_card.py
+++ project2/credit_card.py
@@ -54,44 +54,36 @@ print("Data Types:", train_data.dtypes)
 
 print(' {} - rows \n {} - columns.\n'.format(train_data.shape[0],train_data.shape[1]))
 
-# Find the total number of missing values in the dataframe
+# Nadjemo vrijednosti koje nam fale
 print ("\nMissing values :  ", train_data.isnull().sum().values.sum())
 
-# printing total numbers of Unique value in the dataframe.
+# Ukupan broj unikatnih vrijednosti
 print ("\nUnique values :  \n", train_data.nunique())
 
-# print data to see which one are missing
+# Ispisemo podatke da vidimo koje nam vrijednosti fale
 print ("\nMissing values :  \n", train_data.isnull().any())
 
-# CREDIT_LIMIT  and MINIMUM_PAYMENTS has missing values so we need to remove with median
+# CREDIT_LIMIT  i MINIMUM_PAYMENTS nemaju sve vrijednosti, pa ih popunjamavo medianom
 train_data['CREDIT_LIMIT'].fillna(train_data['CREDIT_LIMIT'].median())
 train_data['CREDIT_LIMIT'].count()
 train_data['MINIMUM_PAYMENTS'].median()
 train_data['MINIMUM_PAYMENTS'].fillna(train_data['MINIMUM_PAYMENTS'].median())
 
-# checking the replacement
+# Provjeravamo
 print("\nMissing values :  \n", train_data.isnull().any())
 
 train_clean = train_data.apply(lambda x:x.fillna(x.value_counts().index[0]))
 train_clean = train_clean.drop(['CUST_ID'], axis=1)
 
-# StandardScaler performs the task of Standardization. Usually a dataset contains variables that are different in scale.
-scaler = StandardScaler()
+# MinMaxScaler skalira podatke pojedinacno tako da su u su opsegu izmedju 0 i 1
+scaler = MinMaxScaler()
 train_data_scl= scaler.fit_transform(train_clean.values)
 train_clean = pd.DataFrame(train_data_scl,columns=train_clean.columns)
 
-#train_clean = pd.DataFrame(x_scaled,columns=train_clean.columns)
 
-# Normalization refers to rescaling real valued numeric attributes into the range 0 and 1.
-# It is useful to scale the input attributes for a model that relies on the magnitude of values, such as distance measures used in unsupervised learning.
-norm = normalize(train_data_scl)
-
-# We can apply both (StandartScaler and Normalize) on our data before clustering.
-df_norm=pd.DataFrame(norm)
-
-
-
-#Kmeans
+#Elbow metoda da nadjemo optimalan broj klastera - dobijalo grafik koji izgleda kao savijen lakat
+#Kako povecavamo broj klastera, varijacija ce se smanjivati sve dok ne dodjemo do tacke gdje
+#povecavanje klastera ne rezultuje vecim modelom
 Sum_of_squared_distances = []
 K = range(1, 15)
 for k in K:
@@ -107,10 +99,11 @@ y_pos = 140000
 plt.text(x_pos, y_pos, 'After 8 clusters adding more gives minimal benefit to the model.')
 plt.show()
 
+#Kmeans nad optimalnih 8 klastera da bi prikazali na graficima
 km = KMeans(n_clusters=8)
 km = km.fit_predict(train_clean)
 
-# 3D data
+# 3D data prikaz klastera
 pca = PCA(n_components=3).fit_transform(train_clean)
 fig = plt.figure(figsize=(8, 7), dpi=80, facecolor='w', edgecolor='k')
 ax = plt.axes(projection="3d")
@@ -121,23 +114,30 @@ zLabel = ax.set_ylabel('Z')
 plt.show()
 print(Sum_of_squared_distances)
 
-# 2D data
+# 2D data prikaz klastera
 pca2 = PCA(n_components=2).fit_transform(train_clean)
 plt.scatter(pca2[:, 0], pca2[:, 1], c=km, s=50, cmap='viridis')
 plt.show()
 
+# Dijelimo na test podatke i train podatke
 np.random.seed(0)
 msk = np.random.rand(len(train_clean)) < 0.8
 trained_data = train_clean[msk]
 test_data = train_clean[~msk]
 
+#Pretvaramo u numpy array
 data = np.array(trained_data)
 data_test = np.array(test_data)
 
+#Kmeans
 kmeans = KMeans(n_clusters=8, random_state=0).fit(data)
 
+#koristeci istrenirani model radimo predict nad testnim podacima
 y_k = kmeans.predict(data_test)
+#Predict podatke stavljamo u novu kolonu
 test_data['PREDICTED_CLUSTER'] = y_k
+
+#Koristimo pandas groupby da analiziramo klastere
 train_summary = test_data.groupby(by='PREDICTED_CLUSTER').mean()
 train_summary = train_summary[['BALANCE', 'PURCHASES',
                                'PURCHASES_FREQUENCY','CREDIT_LIMIT',
